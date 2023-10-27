aa6e0634cf506da2e05e159bf63a3902db07789b
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2023-10-27 10:50:09 +0200

    Support trusted_tenant: scope

--- connector/hsdp/extend_payload.go
+++ connector/hsdp/extend_payload.go
@@ -14,6 +14,8 @@ func (c *HSDPConnector) ExtendPayload(scopes []string, payload []byte, cdata []b
 
 	c.logger.Info("ExtendPayload called")
 
+	trustedOrgID := c.trustedOrgID
+
 	if err := json.Unmarshal(cdata, &cd); err != nil {
 		return payload, err
 	}
@@ -24,6 +26,21 @@ func (c *HSDPConnector) ExtendPayload(scopes []string, payload []byte, cdata []b
 		if scope == "federated:id" {
 			originalClaims["iam_access_token"] = string(cd.AccessToken)
 		}
+		// Check if we should use the trusted_tenant scope
+		if strings.HasPrefix(scope, "trusted_tenant:") {
+			var tenant string
+			if count, err := fmt.Sscanf(scope, "trusted_tenant:%s", &tenant); err == nil && count == 1 {
+				trustedOrgID = tenant
+			} else {
+				c.logger.Errorf("error parsing scope: %s", scope)
+			}
+		}
+
+		// Experimental fill introspect body into claims
+		if scope == "hsp:iam:introspect" {
+			originalClaims["introspect"] = cd.Introspect
+		}
+
 		// Experimental tenant scoping
 		if strings.HasPrefix(scope, "tenant:") {
 			group := strings.TrimPrefix(scope, "tenant:")
@@ -43,16 +60,18 @@ func (c *HSDPConnector) ExtendPayload(scopes []string, payload []byte, cdata []b
 			}
 		}
 	}
-	originalClaims["moid"] = cd.Introspect.Organizations.ManagingOrganization
+	originalClaims["mid"] = cd.Introspect.Organizations.ManagingOrganization
+	originalClaims["tid"] = trustedOrgID
 	// Rewrite subject
 	var orgSubs []string
+	var orgGroups []string
 	for _, org := range cd.Introspect.Organizations.OrganizationList {
-		if org.OrganizationID != cd.TrustedIDPOrg {
-			continue
-		}
-		for _, group := range org.Groups {
-			if strings.HasPrefix(group, "sub-") {
-				orgSubs = append(orgSubs, fmt.Sprintf("sub:%s", strings.TrimPrefix(group, "sub-")))
+		if org.OrganizationID == trustedOrgID { // Add groups from trusted IDP org
+			orgGroups = org.Groups
+			for _, group := range org.Groups {
+				if strings.HasPrefix(group, "sub-") {
+					orgSubs = append(orgSubs, fmt.Sprintf("sub:%s", strings.TrimPrefix(group, "sub-")))
+				}
 			}
 		}
 	}
@@ -68,6 +87,9 @@ func (c *HSDPConnector) ExtendPayload(scopes []string, payload []byte, cdata []b
 		subs := strings.Join(orgSubs, ":")
 		origSub := originalClaims["sub"].(string)
 		originalClaims["sub"] = fmt.Sprintf("%s:id:%s", subs, origSub)
+	}
+	if len(orgGroups) > 0 || trustedOrgID != cd.TrustedIDPOrg {
+		originalClaims["groups"] = orgGroups
 	}
 
 	extendedPayload, err := json.Marshal(originalClaims)
